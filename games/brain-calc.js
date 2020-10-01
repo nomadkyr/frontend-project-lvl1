47cd0d74dd22deb6ab717cba3217ec726b2394fc
Daria <dariasid04@gmail.com>
2020-10-01 22:15:18 +0600

    step 6

---
+++ games/brain-calc.js
@@ -0,0 +1,41 @@
+import readlineSync from 'readline-sync';
+
+const brainCalc = () => {
+  console.log('Welcome to the Brain Games!');
+  const printName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${printName}!`);
+  const message = 'What is the result of the expression?';
+  console.log(message);
+
+  for (let i = 0; i < 3; i += 1) {
+    const firstNumber = Math.floor(Math.random() * 10) + 1;
+    const secondNumber = Math.floor(Math.random() * 10) + 1;
+    const operators = ['+', '-', '*'][Math.floor(Math.random() * 3)];
+
+    let result;
+    switch (operators) {
+      case '+':
+        result = firstNumber + secondNumber; break;
+      case '-':
+        result = firstNumber - secondNumber; break;
+      case '*':
+        result = firstNumber * secondNumber; break;
+      default:
+        return null;
+    }
+
+    const question = `${'Question: '}${firstNumber} ${operators} ${secondNumber}`;
+    console.log(question);
+    const printAnswer = readlineSync.question('Your answer: ');
+    if (printAnswer == result) {
+      console.log('Correct');
+    } else {
+      const failAnswer = `"${printAnswer}" is wrong answer ;(. Correct answer was "${result}"\n Let's try again, ${printName}!`;
+      return failAnswer;
+    }
+  }
+
+  console.log(`Congratulations, ${printName}!`);
+};
+
+export default brainCalc;
--- src/cli.js
+++
@@ -1,6 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const printName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${printName}!`);
-
-export default printName;
